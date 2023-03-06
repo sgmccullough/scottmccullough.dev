3c7a47921bbf8add9422e561ae231d9db031d86a
Scott McCullough <mcculloughsco12@gmail.com>
2023-03-06 15:58:53 +0000

    First pass

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders GitHub link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/GitHub/i);
   expect(linkElement).toBeInTheDocument();
 });
